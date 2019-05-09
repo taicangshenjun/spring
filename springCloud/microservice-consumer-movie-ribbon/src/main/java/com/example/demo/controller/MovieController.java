34c6c8183ea16e7b62bb46ffabc2e40e0f0f457f
taicangshenjun <45221460+taicangshenjun@users.noreply.github.com>
2019-05-09 22:15:53 +0800

    1
    1

---
+++ springCloud/microservice-consumer-movie-ribbon/src/main/java/com/example/demo/controller/MovieController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.entity.User;
+
+@RestController("movieController")
+@RequestMapping("/movieController")
+public class MovieController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
+	/**
+	 * request头,application/json;charset=UTF-8返回json
+	 */
+	@GetMapping(value = "/user/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public User findById(@PathVariable("id") String id) {
+		String url = "http://microservice-provider-user/userController/findById/" + id;
+		return this.restTemplate.getForObject(url, User.class);
+	}
+	
+	@GetMapping("/log-instance")
+	public void logUserInstance() {
+		ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+		logger.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+	}
+	
+	@GetMapping("/user-instance")
+	public List<ServiceInstance> showInfo(){
+		return this.discoveryClient.getInstances("microservice-provider-user");
+	}
+	
+}
---
+++ springCloud/microservice-consumer-movie-ribbon/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,55 @@
+package com.example.demo.entity;
+
+public class User {
+
+	private String id;
+	
+	private String userName;
+	
+	private String name;
+	
+	private Integer age;
+	
+	private Double balance;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	
+}
